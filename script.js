eded9b8b16ab9bde23022a7b9c78becae6c4eb12
SerBEEan <sergey.atwork7@gmail.com>
2021-02-23 23:41:59 +0300

    Fix bugs

--- script.js
+++ script.js
@@ -1,35 +1,57 @@
 $( function() {
-  const buttonColor = $("button[name=color]");
-  const buttonBackground = $("button[name=background]");
+  const buttonColor = $( "button[name=color]" );
+  const buttonBackground = $( "button[name=background]" );
+  const blockShow = $( "#show" );
+  const slider1 = $( "#slider1" );
+  const slider2 = $( "#slider2" );
+  const slider3 = $( "#slider3" );
 
-  let toggleProperty = "color"
+  const history = {
+    color: { r: 0, g: 0, b: 0 },
+    background: { r: 255, g: 255, b: 255 }
+  };
+
+  let toggleProperty = "color";
+  let lastProperty = "background";
+
   let lastButton = buttonColor;
-  createSlider.call(lastButton, toggleProperty);
 
 
-  buttonColor.on('click', createSlider.bind(buttonColor, "color"))
-  buttonBackground.on('click', createSlider.bind(buttonBackground, "background"))
+  buttonColor.on("click", updateSlider.bind(buttonColor, "color"))
+  buttonBackground.on("click", updateSlider.bind(buttonBackground, "background"))
 
-  function createSlider(property) {
-    $( ".slider" )
-      .slider({
-        min: 0,
-        max: 255,
-        range: "min",
-        change: function( event, ui ) {
-          $( "#show" ).css(property, `rgb(
-            ${ $( "#slider1" ).slider( "value" ) },
-            ${ $( "#slider2" ).slider( "value" ) },
-            ${ $( "#slider3" ).slider( "value" ) })`
-          );
-        }
-      });
+  $( ".slider" )
+    .slider({
+      min: 0,
+      max: 255,
+      range: "min",
+      change: function( event, ui ) {
+        blockShow.css(toggleProperty, `rgb(
+          ${ slider1.slider("value") },
+          ${ slider2.slider("value") },
+          ${ slider3.slider("value") })`
+        );
+      }
+    });
+
+  buttonColor.addClass("ui-state-active");
+
+
+  function updateSlider(property) {
+    history[toggleProperty].r = slider1.slider("value");
+    history[toggleProperty].g = slider2.slider("value");
+    history[toggleProperty].b = slider3.slider("value");
 
     this.addClass("ui-state-active");
-    lastButton !== this && lastButton.removeClass("ui-state-active");
+    lastButton.removeClass("ui-state-active");
     lastButton = this;
 
+    lastProperty = toggleProperty;
     toggleProperty = property;
+
+    slider1.slider("value", history[toggleProperty].r);
+    slider2.slider("value", history[toggleProperty].g);
+    slider3.slider("value", history[toggleProperty].b);
   }
 
 });
